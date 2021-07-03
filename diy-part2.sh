7f334a0256977d3e285a0241eac9bd02b64b094c
qxxwwyy <52125229+qxxwwyy@users.noreply.github.com>
2021-07-03 21:40:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,4 +13,8 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-git clone https://github.com/coolsnowwolf/lede/tree/master/package/lean
+https://github.com/coolsnowwolf/lede/tree/master/package/lean/luci-app-unblockmusic
+https://github.com/coolsnowwolf/lede/tree/master/package/lean/luci-app-zerotier
+https://github.com/coolsnowwolf/lede/tree/master/package/lean/luci-theme-netgear
+https://github.com/coolsnowwolf/lede/tree/master/package/lean/luci-app-softethervpn
+
