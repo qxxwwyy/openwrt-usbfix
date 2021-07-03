8837a0023769255c39adecfdcf96439aaf62b6c2
qxxwwyy <52125229+qxxwwyy@users.noreply.github.com>
2021-07-03 21:25:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-git clone https://github.com/coolsnowwolf/lede/tree/master/package/lean
+
